8cdd26c19ab0e5ef34538e6993cfbca28ede5f18
rustam-1999 <tolliboyevrustam1999@gmail.com>
2022-12-18 01:02:45 +0500

    add init filims

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// var users = [
+// 	{
+// 		name: 'Abdulloh',
+// 		debt: 10000,
+// 	},
+// 	{
+// 		name: 'Bekzod',
+// 		debt: 20000,
+// 	},
+// 	{
+// 		name: 'Rustam',
+// 		debt: 35000,
+// 	},
+// 	{
+// 		name: 'Jamshid',
+// 		debt: 50000,
+// 	},
+// 	{
+// 		name: 'Bunyod',
+// 		debt: 5000,
+// 	},
+// ];
+// let array = users.reduce((debt, el, index, arr) => {
+// 	debt += el.debt / arr.length;
+// 	return debt;
+// }, 0);
+// console.log(users);
+
+
+// --------------------------------------
+// var elForm = document.querySelector('.js-form')
+// var elInput = document.querySelector('.js-input')
+// var elList = document.querySelector('.js-list')
+
+
+// console.log(films);
